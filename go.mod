795cdf656a86730999f52a31fab646193f64c9af
Avei20 <muhammadabdulazizalghofari@gmail.com>
2024-03-06 22:13:13 +0700

    feat: FUCKING COMPLETED SETUP FOR SINGLE SOURCE PROTO REPOSITORY

--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.21.4
 require (
 	github.com/google/uuid v1.4.0
 	google.golang.org/grpc v1.61.0
-	google.golang.org/protobuf v1.32.0
+	google.golang.org/protobuf v1.33.0
 )
 
 require (
+	buf.build/gen/go/avei/proto/grpc/go v1.3.0-20240218021503-1145c85488ef.2 // indirect
+	buf.build/gen/go/avei/proto/protocolbuffers/go v1.33.0-20240218021503-1145c85488ef.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	golang.org/x/net v0.18.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
--- server/main.go
+++ server/main.go
@@ -5,7 +5,11 @@ import (
 	"log"
 	"net"
 
-	pb "avei-grpc/proto"
+	// pb "buf.build/gen/go/avei/proto/protocolbuffers/go/todo/v1"
+	pb "buf.build/gen/go/avei/proto/grpc/go/todo/v1/todov1grpc"
+	// "buf.build/gen/go/avei/proto/protocolbuffers/go"
+
+	v1 "buf.build/gen/go/avei/proto/protocolbuffers/go/todo/v1"
 
 	"github.com/google/uuid"
 	"google.golang.org/grpc"
@@ -15,14 +19,23 @@ const (
 	PORT = ":50051"
 )
 
+// moduleOwner = avei 
+// moduleName = proto 
+// pluginOwner = protocolbuffers 
+// pluginName = go 
+
+
+
+
+
 type TodoServer struct {
-	pb.UnimplementedTodoServiceServer
+	pb.TodoServiceServer
 }
 
-func (s* TodoServer) CreateTodo (ctx context.Context, in *pb.NewTodo) (*pb.Todo, error) {
+func (s* TodoServer) CreateTodo (ctx context.Context, in *v1.CreateTodoRequest) (*v1.CreateTodoResponse, error) {
 	log.Printf("Received: %v", in.GetName())
 
-	todo := &pb.Todo{
+	todo := &v1.CreateTodoResponse{
 		Name :  in.GetName(),
 		Description : in.GetDescription(),
 		Done: false,
